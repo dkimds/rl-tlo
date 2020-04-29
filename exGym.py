8847302904956c44630fad0bf4a07ad11f15da19
Daeseong <dkimds@gmail.com>
2020-04-29 16:43:52 +0900

    register 추가

--- exGym.py
+++ exGym.py
@@ -1,5 +1,16 @@
 import gym
-env = gym.make("FrozenLake-v0")
+from gym.envs.registration import register
+
+register(
+    id='FrozenLake-v3',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={
+        'map_name': '4x4',
+        'is_slippery': False
+    }
+)
+
+env = gym.make("FrozenLake-v3")
 
 num_episodes = 1000
 for i in range(num_episodes):
