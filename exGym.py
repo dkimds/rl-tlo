67b31b703564b7569f414d8031a021c39d8e533c
Daeseong <dkimds@gmail.com>
2020-04-29 17:04:50 +0900

    디버그 코드 추가

--- exGym.py
+++ exGym.py
@@ -2,6 +2,7 @@ import gym
 from gym.envs.registration import register
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 register(
     id='FrozenLake-v3',
@@ -16,16 +17,31 @@ env = gym.make("FrozenLake-v3")
 
 Q = np.zeros([env.observation_space.n, env.action_space.n])
 
+num_episodes = 1000
+rList = []
+successRate = []
+
 def rargmax(vector):
     m = np.amax(vector)
     indices = np.nonzero(vector == m)[0]
     return random.choice(indices)
 
-num_episodes = 1000
 for i in range(num_episodes):
     state = env.reset()
+    total_reward = 0
     done=None
     while not done:
         action = rargmax(Q[state,:])
         new_state, reward, done, _ =env.step(action)
         Q[state,action] = reward + np.max(Q[new_state,:])
+        total_reward += reward
+        state = new_state
+    rList.append(total_reward)
+    successRate.append(sum(rList)/(i+1))
+
+print("Final Q-Table")
+print(Q)
+print("Success Rate : ", successRate[-1])
+plt.plot(range(len(successRate)), successRate) # 성공률 그래프
+plt.plot(range(len(rList)), rList)             # reward 그래프
+plt.show()
