2783fd36640146bc22ef14c8f15fc12243b1186a
Daeseong <dkimds@gmail.com>
2020-04-29 13:49:09 +0900

    print로 각상태 모니터링

--- exGym.py
+++ exGym.py
@@ -3,10 +3,13 @@ env = gym.make("FrozenLake-v0")
 
 num_episodes = 1000
 for i in range(num_episodes):
-    env.reset()
+    state = env.reset()
     done=None
     while not done:
         #env.render()
         action = env.action_space.sample()
         state, reward, done, _ =env.step(action)
     env.render()
+    print(state)
+
+env.close()
