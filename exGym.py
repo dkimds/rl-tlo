43160b98c98a3ab181ef1f029f4a4c8b09522c39
Daeseong <dkimds@gmail.com>
2020-05-01 17:01:16 +0900

    Leaning on stochastic world.

--- exGym.py
+++ exGym.py
@@ -1,27 +1,28 @@
 import gym
-from gym.envs.registration import register
+# from gym.envs.registration import register
 import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-register(
-    id='FrozenLake-v3',
-    entry_point='gym.envs.toy_text:FrozenLakeEnv',
-    kwargs={
-        'map_name': '4x4',
-        'is_slippery': False
-    }
-)
+# register(
+#     id='FrozenLake-v3',
+#     entry_point='gym.envs.toy_text:FrozenLakeEnv',
+#     kwargs={
+#         'map_name': '4x4',
+#         'is_slippery': False
+#     }
+# )
 
-env = gym.make("FrozenLake-v3")
+env = gym.make("FrozenLake-v0")
 
 Q = np.zeros([env.observation_space.n, env.action_space.n]) # (16, 4): 4*4 map + 상하좌우 4개
 
-num_episodes = 1000
+num_episodes = 2000
 rList = []
 successRate = []
 
 ###e = 0.1 # exploit & exploration
+learning_rate = 0.8
 r = 0.9 # discount rate
 
 def rargmax(vector):
@@ -41,7 +42,8 @@ for i in range(num_episodes):   # num_episodes회 학습하고, 업데이트
         action = rargmax(
             Q[state,:] + np.random.rand(env.action_space.n)/(i+1))
         new_state, reward, done, _ =env.step(action)
-        Q[state,action] = reward + r * np.max(Q[new_state,:])
+        Q[state,action] = (1 - learning_rate) * Q[state, action] + \
+            learning_rate * (reward + r * np.max(Q[new_state,:]))
         total_reward += reward
         state = new_state
     rList.append(total_reward)                 # 이번 게임에서 보상이 0인지 1인지
