9ef77cbf736ba474a8aed4ece3daf1488bcc78e3
Daeseong <dkimds@gmail.com>
2020-05-01 15:59:43 +0900

    E-greedy 적용

--- exGym.py
+++ exGym.py
@@ -15,29 +15,36 @@ register(
 
 env = gym.make("FrozenLake-v3")
 
-Q = np.zeros([env.observation_space.n, env.action_space.n])
+Q = np.zeros([env.observation_space.n, env.action_space.n]) # (16, 4): 4*4 map + 상하좌우 4개
 
 num_episodes = 1000
 rList = []
 successRate = []
 
-def rargmax(vector):
-    m = np.amax(vector)
-    indices = np.nonzero(vector == m)[0]
-    return random.choice(indices)
+e = 0.1 # exploit & exploration
+r = 0.9 # discount rate
 
-for i in range(num_episodes):
-    state = env.reset()
-    total_reward = 0
-    done=None
+def rargmax(vector):
+    m = np.amax(vector)                     # Return the maximum of an array or maximum along an axis (0 or 1)
+    indices = np.nonzero(vector == m)[0]    # np.nonzero(True/False vector) -> 최대값인 요소들만 걸러내
+    return random.choice(indices)           # 랜덤으로 하나 선택
+
+for i in range(num_episodes):   # num_episodes회 학습하고, 업데이트 
+    state = env.reset()         # 리셋
+    total_reward = 0            # 그래프 그리기용(성공: 1, 실패: 0)
+    done = None
     while not done:
-        action = rargmax(Q[state,:])
+        rand = random.random()
+        if (rand < e):
+            action = env.action_space.sample() # 완전 랜덤 선택
+        else:
+            action = rargmax(Q[state,:])
         new_state, reward, done, _ =env.step(action)
-        Q[state,action] = reward + np.max(Q[new_state,:])
+        Q[state,action] = reward + r * np.max(Q[new_state,:])
         total_reward += reward
         state = new_state
-    rList.append(total_reward)
-    successRate.append(sum(rList)/(i+1))
+    rList.append(total_reward)                 # 이번 게임에서 보상이 0인지 1인지
+    successRate.append(sum(rList)/(i+1))       # 지금까지 성공률 
 
 print("Final Q-Table")
 print(Q)
